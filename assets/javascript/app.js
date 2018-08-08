0efba120d47c2be5993294542c4a75f9928332b5
riggs <riggs041@gmail.com>
2018-08-08 02:05:45 -0500

    updated files

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+function linkedin() {
+    window.open("https://www.linkedin.com/in/michael-riggs-041");
+}
+
+function github() {
+    window.open("https://github.com/Riggs041");
+}
+
+function project1() {
+    window.open("https://riggs041.github.io/Project1/");
+}
+
+function project2() {
+    window.open("https://scareadvisor.herokuapp.com/");
+}
